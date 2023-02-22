4534cd14adcd9e00cbb5271253ef6be7ef734939
Carmen <33059@ma-web.nl>
2023-02-22 20:26:05 +0100

    video 12

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
-const color = document.getElementById("js--color--1");
+const colors = document.getElementsByClassName("colors__color");
+let secondColor = false;
 
-color.onclick = function(){
-    color.children.classList.add("colors__circle--selected");
+
+for(let i = 0; i < colors.length; i++ ){
+    // Set Color
+
+    if(secondColor === false){
+        colors[i].children[0].style.background = "#BAD7E9";
+        secondColor = true;
+    }
+    else{
+        colors[i].children[0].style.background = "#4E31AA";
+        secondColor = false;
+    }
+
+
+    // Onclick
+    colors[i].onclick = function(){
+        colors[i].children[0].classList.add("colors__circle--selected");
+        navigator.clipboard.writeText(colors[i].children[0].style.background);
+        document.title = colors[i].children[0].style.background;
+    }
 }
